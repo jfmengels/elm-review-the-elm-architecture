3b387cde5201d92f266c79c6c7be7f8088a9858a
Jeroen Engels <jfm.engels@gmail.com>
2020-09-04 23:47:11 +0200

    Revert "Remove try it out sections"
    This reverts commit a1ccabf8fb765a79bec0c06da0e6b29ec32de32e.

--- src/NoMissingSubscriptionsCall.elm
+++ src/NoMissingSubscriptionsCall.elm
@@ -52,6 +52,15 @@ This won't fail if `SomeModule` does not define a `subscriptions` function.
     subscriptions model =
         SomeModule.subscriptions
 
+
+## Try it out
+
+You can try this rule out by running the following command:
+
+```bash
+elm-review --template jfmengels/elm-review-the-elm-architecture/example --rules NoMissingSubscriptionsCall
+```
+
 -}
 rule : Rule
 rule =
